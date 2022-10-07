5a293d8df9f5553fc46a730db11be06854e0115d
Meenal <114145036+meenalpannase@users.noreply.github.com>
2022-10-07 12:49:45 +0530

    first commit

--- master_pkg.sv
+++ master_pkg.sv
@@ -3,11 +3,6 @@
 package master_pkg;
    `include "uvm_macros.svh"
    import uvm_pkg::*;
-   `include "m_seq_item.sv"
-   `include "m_sequence.sv"
-   `include "m_sequencer.sv"
-   `include "m_driver.sv"
-   `include "m_monitor.sv"
-   `include "m_agent.sv"
+  //include master files
 endpackage
 `endif
